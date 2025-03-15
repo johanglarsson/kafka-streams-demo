9a5bb1ff5ab7d74c044723dc8a18db491fd70cf9
Johan Larsson <jola04@gmail.com>
2025-03-15 09:22:24 +0000

    feat: initial spring boot version

---
+++ src/test/java/se/starlight/ApplicationTest.java
@@ -0,0 +1,13 @@
+package se.starlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+    }
+}
